71478630a1f3844d37618a778a44879d5eb49051
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2024-02-09 13:58:05 -0300

    controller unidade

--- Models/Unit.cs
+++ Models/Unit.cs
@@ -9,6 +9,7 @@ public class Unit
     public bool PeopleLiving { get; set; }
     public string Observation { get; set; }
     public bool HasGarage { get; set; }
+    public int BlockId { get; set; }
 
 }
 
