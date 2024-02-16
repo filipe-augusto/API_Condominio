cd64ce73be0d596c14365ff806fac1206a0d17b4
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2024-02-16 14:23:19 -0300

    adicionando crud para o block

---
+++ Controllers/BlockController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API_Condominio.Data;
+using API_Condominio.Extensions;
+using API_Condominio.Models;
+using API_Condominio.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+namespace API_Condominio.Controllers;
+
+    [ApiController]
+    public class BlockController : Controller
+    {
+    [HttpGet("v1/blocks")]
+    public async Task<IActionResult> GetAsyncCache(
+  [FromServices] IMemoryCache cache,
+  [FromServices] DataContext context)
+    {
+        try
+        {
+            var blocks = cache.GetOrCreate("BlockCache", entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
+                return GetBocks(context);
+            });
+            return Ok(new ResultViewModel<List<Block>>(blocks));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Block>>("05X04 - Falha interna no servidor"));
+        }
+    }
+    private List<Block> GetBocks(DataContext context) => context.Blocks.ToList();
+
+
+
+
+    [Authorize(Roles = "admin")]
+        [HttpGet("v1/blocks/")]
+        public async Task<IActionResult> Get([FromServices] DataContext context)
+        {
+
+            try
+            {
+                var blocks = await context.Blocks.ToListAsync();
+                return Ok(blocks);
+            }
+            catch
+            {
+                return StatusCode(500, "05x04 - Falha Interna no serivodor");
+            }
+        }
+
+
+    [Authorize(Roles = "admin")]
+    [HttpGet("v1/blocks/{id:int}")]
+    public async Task<IActionResult> GetId(
+        [FromServices] DataContext context,
+        [FromRoute] int id)
+    {
+        try
+        {
+            var block = await context.Blocks.FirstOrDefaultAsync(y => y.Id == id);
+            if (block == null)
+                NotFound("Conteúdo não encontrado");
+            return Ok(block);
+        }
+        catch 
+        {
+
+            return StatusCode(500, "05x04 - Falha interna no servidor");
+        }
+    }
+
+
+
+    [Authorize(Roles ="admin")]
+    [HttpPost("v1/blocks/")]
+    public async Task<IActionResult> Post([FromServices] DataContext context, [FromBody] BlockViewModel model)
+    {
+        try
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var block = new Block
+            {
+                NameBlock = model.NameBlock,
+                QuantityeUnit = model.QuantityeUnit,
+                QuantityFloor = model.QuantityFloor,
+            };
+            await context.Blocks.AddAsync(block);
+            await context.SaveChangesAsync();
+
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                identificacaoBlock = model.NameBlock,
+                QuantidadeUnidades = model.QuantityeUnit,
+                QuantidadeAndares = model.QuantityFloor,
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("05X99 - Este E-mail já está cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+
+    [Authorize(Roles = "admin")]
+    [HttpPut("v1/blocks/{id:int}")]
+    public async Task<IActionResult> Put([FromServices] DataContext context,
+        [FromBody] BlockViewModel model, [FromRoute] int id)
+    {
+        try {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var block = await context.Blocks.FirstOrDefaultAsync(x => x.Id == id);
+            if (block == null)
+                return BadRequest(new ResultViewModel<Unit>(ModelState.GetErrors()));
+
+            block.NameBlock = model.NameBlock;
+            block.QuantityFloor = model.QuantityFloor;
+            block.QuantityeUnit = model.QuantityeUnit;
+
+            await context.Blocks.AddAsync(block);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                identificacaoBlock = model.NameBlock,
+                QuantidadeUnidades = model.QuantityeUnit,
+                QuantidadeAndares = model.QuantityFloor,
+            }));
+
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("05X99 - Este E-mail já está cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+
+
+    [Authorize(Roles = "admin")]
+    [HttpPost("v1/blocks/{id:int}")]
+    public async Task<IActionResult> DeleteAsync([FromServices] DataContext context, [FromRoute] int id)
+    {
+        try
+        {
+            var block = await context.Blocks.FirstOrDefaultAsync(x => x.Id ==id);
+            if (block == null)
+                return BadRequest(new ResultViewModel<string>("Conteúdo não encontrado"));
+
+            context.Blocks.Remove(block);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/blocks/{block.Id}", block);
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(500, "05x13 - Não foi possivel deletar a categoria");
+        }
+        catch
+        {
+            return StatusCode(500, "05x14 - falha interna no servidor");
+        }
+    }
+
+  
+
+}
+
---
+++ ViewModel/BlockViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Condominio.ViewModel
+{
+    public class BlockViewModel
+    {
+        [Required(ErrorMessage = "O nome do bloco é obrigatorio")]
+        public string NameBlock { get; set; }
+        [Required(ErrorMessage = "Quantidade de unidades é obrigatorio informar")]
+        [MinLength(1, ErrorMessage = "Quantidade minima é 1")]
+        public int QuantityeUnit { get; set; }
+        [Required(ErrorMessage = "Quantidade de andares é obrigatorio informar")]
+        [MinLength(1, ErrorMessage ="Quantidade minima é 1")]
+        [MaxLength(1, ErrorMessage = "Quantidade maxima é 100")]
+        public int QuantityFloor { get; set; }
+
+    }
+}
+
+
+
